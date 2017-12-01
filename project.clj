447bccdede54e7da003d166ed0eacf1b9ce8e473
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-12-01 10:59:47 -0300

    Start the advent calendar

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject advent2017 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [madstap/comfy "1.0.4"]
+                 [medley "1.0.0"]])
