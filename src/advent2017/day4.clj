47aaac51d63b764f3e614f84de0740ce0a157d63
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-12-04 19:47:30 -0300

    Day4 part2

--- src/advent2017/day4.clj
+++ src/advent2017/day4.clj
@@ -37,3 +37,33 @@
   (solve1 input) ;=> 383
 
   )
+
+;; --- Part Two ---
+
+;; For added security, yet another system policy has been put in
+;; place. Now, a valid passphrase must contain no two words that are
+;; anagrams of each other - that is, a passphrase is invalid if any
+;; word's letters can be rearranged to form any other word in the
+;; passphrase.
+
+;; For example:
+
+;; abcde fghij is a valid passphrase.
+;; abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+;; a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+;; iiii oiii ooii oooi oooo is valid.
+;; oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+;; Under this new system policy, how many passphrases are valid?
+
+(defn valid2? [passphrase]
+  (apply distinct? (map frequencies passphrase)))
+
+(defn solve2 [in]
+  (count (filter valid2? in)))
+
+(comment
+
+  (solve2 input) ;=> 265
+
+  )
