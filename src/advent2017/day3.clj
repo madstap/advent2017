c68c9cbd171635612087685c7364ee49c1ba760d
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-12-04 19:17:22 -0300

    Day3 part2

--- src/advent2017/day3.clj
+++ src/advent2017/day3.clj
@@ -95,3 +95,56 @@
   (solve1 input) ;=> 371
 
   )
+
+;; --- Part Two ---
+
+;; As a stress test on the system, the programs here clear the grid
+;; and then store the value 1 in square 1. Then, in the same
+;; allocation order as shown above, they store the sum of the values
+;; in all adjacent squares, including diagonals.
+
+;; So, the first few squares' values are chosen as follows:
+
+;; Square 1 starts with the value 1.
+;; Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+;; Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+;; Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+;; Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+;; Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+;; 147  142  133  122   59
+;; 304    5    4    2   57
+;; 330   10    1    1   54
+;; 351   11   23   25   26
+;; 362  747  806--->   ...
+
+;; What is the first value written that is larger than your puzzle input?
+
+(defn neighbors [[x y]]
+  #{[(dec x) y] [(inc x) y] [(inc x) (inc y)] [(inc x) (dec y)]
+    [x (dec y)] [x (inc y)] [(dec x) (dec y)] [(dec x) (inc y)]})
+
+(def spiral
+  "A sequence of the coordinates in the spiral."
+  (cons [0 0] (mapcat (fn [n]
+                        (reverse (backwards-square [n (- n)])))
+                      (rest (range)))))
+
+(defn solve2 [in]
+  (reduce (fn [memory coords]
+            (let [v (apply + (keep memory (neighbors coords)))]
+              (if (> v in)
+                (reduced v)
+                (assoc memory coords v))))
+          {[0 0] 1}
+          (rest spiral)))
+
+(deftest two
+  (is (= [1 2 4 5 10 11 23 25 26 54 57 59 122 133 142 147 304 330 351 362 747 806]
+         (take 22 (iterate solve2 1)))))
+
+(comment
+
+  (solve2 input) ;=> 369601
+
+  )
