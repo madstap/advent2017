27932420e209852c61bbd9b64c0ff78f7164f3b4
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-12-07 21:07:09 -0300

    day6 part 2

--- src/advent2017/day6.clj
+++ src/advent2017/day6.clj
@@ -102,3 +102,34 @@
   (solve1 input) ;=> 6681
 
   )
+
+;; --- Part Two ---
+
+;; Out of curiosity, the debugger would also like to know the size of
+;; the loop: starting from a state that has already been seen, how
+;; many block redistribution cycles must be performed before that same
+;; state is seen again?
+
+;; In the example above, 2 4 1 2 is seen again after four cycles, and
+;; so the answer in that example would be 4.
+
+;; How many cycles are in the infinite loop that arises from the
+;; configuration in your puzzle input?
+
+
+(defn solve2 [in]
+  (loop [seen {}, current in, steps 0]
+    (if (contains? seen current)
+      (- steps (get seen current))
+      (recur (assoc seen current steps)
+             (redistribute current)
+             (inc steps)))))
+
+(deftest two
+  (is (= 4 (solve2 [0 2 7 0]))))
+
+(comment
+
+  (solve2 input) ;=> 2392
+
+  )
