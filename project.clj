50573e38bc1c1e977007f5bbbe681836f81122d4
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-12-08 22:52:11 -0300

    day7 part1

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.datomic/datomic-free "0.9.5656"]
+                 [instaparse "1.4.8"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [net.cgrand/xforms "0.15.0"]
                  [madstap/comfy "1.0.4"]
